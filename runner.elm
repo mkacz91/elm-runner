91af00cfbc4b597bca871404bc04627691d3863f
Marcin Kaczmarek <mkacz91@gmail.com>
2015-05-21 01:22:26 +0200

    Basic input

--- runner.elm
+++ runner.elm
@@ -1,23 +1,82 @@
-import Window
-import Signal exposing ((<~), (~), constant)
-import Graphics.Element exposing (container, middle)
-import Graphics.Collage exposing (collage, rect, filled, move)
 import Color exposing (darkBlue, white)
+import Graphics.Element exposing (Element, container, middle)
+import Graphics.Collage exposing (collage, rect, filled, move)
+import Keyboard exposing (KeyCode)
+import Set exposing (Set, member)
+import Signal exposing ((<~), (~), sampleOn, foldp)
+import Time exposing (Time, fps, inSeconds)
+import Window
+
+--------------------------------------------------------------------------------
+-- Configuration                                                              --
+--------------------------------------------------------------------------------
 
 canvasWidth = 600
 canvasHeight = 300
 clearColor = darkBlue
 
-type alias State = {
-  x : Float,
-  y : Float
-}
+--------------------------------------------------------------------------------
+-- Input                                                                      --
+--------------------------------------------------------------------------------
 
-initialState = {
-  x = -10,
-  y = -10
+-- Keyboard input
+
+type Key = Jump | Duck | None
+
+resolveKeys : Set KeyCode -> Key
+resolveKeys keysDown =
+  case (member 38 keysDown, member 40 keysDown) of
+    (True, False) -> Jump
+    (False, True) -> Duck
+    otherwise -> None
+
+key : Signal Key
+key = Signal.map resolveKeys Keyboard.keysDown
+
+-- Time input
+
+time : Signal Float
+time = foldp (+) 0 (inSeconds <~ fps 30)
+
+-- Merged input
+
+type alias Input
+  = {
+    key : Key,
+    time : Float
   }
 
+input : Signal Input
+input = Input <~ key ~ time
+
+--------------------------------------------------------------------------------
+-- State                                                                      --
+--------------------------------------------------------------------------------
+
+type alias State
+  = {
+    x : Float,
+    y : Float
+  }
+
+initialState : State
+initialState
+  = {
+    x = -10,
+    y = -10
+  }
+
+step : Input -> State -> State
+step input state =
+  if input.key == Jump
+    then { state | y <-  10 }
+    else { state | y <- -10 }
+
+--------------------------------------------------------------------------------
+-- Display                                                                    --
+--------------------------------------------------------------------------------
+
+display : (Int, Int) -> State -> Element
 display (w, h) state =
   container w h middle (
     collage canvasWidth canvasHeight [
@@ -26,4 +85,4 @@ display (w, h) state =
     ]
   )
 
-main = display <~ Window.dimensions ~ constant initialState
+main = display <~ Window.dimensions ~ foldp step initialState input
